b276a4a8a751c9f80ac5b68c71eb3a31babdc465
Dhanush Saji <dhanushsaji1@gmail.com>
2023-02-23 17:32:11 +0530

    test

--- Configure/database.js
+++ Configure/database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require('dotenv').config()
 const MONGO = process.env.MONGO
+mongoose.set('strictQuery', false)
 const connection = mongoose.connect(`${MONGO}`)
 
 module.exports = {
